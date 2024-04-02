de18ad9febb1bf5b9b56c5dca94a3848b9d0776c
BraydenBerthold <berthold.15@osu.edu>
2024-04-02 19:24:45 -0400

    Filled Database w/Startup Data

--- src/api/Emerald.Hail.Api/Controllers/CatalogController.cs
+++ src/api/Emerald.Hail.Api/Controllers/CatalogController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Emerald.Hail.Domain.Catalog;
+using Emerald.Hail.Data;
 
 namespace Emerald.Hail.Api.Controllers
 {
@@ -8,20 +9,28 @@ namespace Emerald.Hail.Api.Controllers
     [Route("api/[controller]")]
     public class CatalogController : ControllerBase
     {
+        private readonly StoreContext _context;
+
+        public CatalogController(StoreContext context)
+        {
+            _context = context;
+        }
+
+
         [HttpGet]
         public IActionResult GetItems()
         {
             //return Ok("Hello World!");
-            var items = new[]
-            {
-                new Item(name: "Item 1", description: "Description 1", brand: "Brand 1", price: 100.00m),
-                new Item(name: "Item 2", description: "Description 2", brand: "Brand 2", price: 200.00m),
-                new Item(name: "Item 3", description: "Description 3", brand: "Brand 3", price: 300.00m),
-                new Item(name: "Item 4", description: "Description 4", brand: "Brand 4", price: 400.00m),
-                new Item(name: "Item 5", description: "Description 5", brand: "Brand 5", price: 500.00m)
+            // var items = new[]
+            // {
+            //     new Item(name: "Item 1", description: "Description 1", brand: "Brand 1", price: 100.00m),
+            //     new Item(name: "Item 2", description: "Description 2", brand: "Brand 2", price: 200.00m),
+            //     new Item(name: "Item 3", description: "Description 3", brand: "Brand 3", price: 300.00m),
+            //     new Item(name: "Item 4", description: "Description 4", brand: "Brand 4", price: 400.00m),
+            //     new Item(name: "Item 5", description: "Description 5", brand: "Brand 5", price: 500.00m)
 
-            };
-            return Ok(items);
+            // };
+            return Ok(_context.Items);
 
         }   
 
--- src/api/Emerald.Hail.Data/StoreContext.cs
+++ src/api/Emerald.Hail.Data/StoreContext.cs
@@ -11,4 +11,11 @@ public class StoreContext : DbContext
     }
 
     public DbSet<Item> Items { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        DbInitializer.Initialize(builder);
+    }
+
 }
