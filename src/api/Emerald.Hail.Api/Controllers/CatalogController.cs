7e5bf5d36ab9df58304baed944fe30574ba1a4cb
BraydenBerthold <berthold.15@osu.edu>
2024-04-03 20:11:06 -0400

    Added Startup Data and Updated Controllers

--- src/api/Emerald.Hail.Api/Controllers/CatalogController.cs
+++ src/api/Emerald.Hail.Api/Controllers/CatalogController.cs
@@ -92,24 +92,24 @@ namespace Emerald.Hail.Api.Controllers
             return NoContent();
         }
 
-        // [HttpPut("{id:int}")]
-        // public IActionResult UpdateItem(int id, Item item)
-        // {
-        //     if (id != item.Id)
-        //     {
-        //         return BadRequest();
-        //     }
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateItem(int id, Item item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
 
-        //     var existingItem = _context.Items.Find(id);
-        //     if (existingItem == null)
-        //     {
-        //         return NotFound();
-        //     }
+            var existingItem = _context.Items.Find(id);
+            if (existingItem == null)
+            {
+                return NotFound();
+            }
 
-        //     _context.Entry(existingItem).CurrentValues.SetValues(item);
-        //     _context.SaveChanges();
-        //     return Ok(item);
-        // }
+            _context.Entry(existingItem).CurrentValues.SetValues(item);
+            _context.SaveChanges();
+            return Ok(item);
+        }
 
     }
 
